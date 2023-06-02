decc5275cd2f69624864b91a3114b4dd9d162f9d
Kyle Crawford <kcraw217@gmail.com>
2023-06-02 13:33:52 -0700

    add deletejoin

--- Shop/Controllers/TreatsController.cs
+++ Shop/Controllers/TreatsController.cs
@@ -101,13 +101,13 @@ namespace Shop.Controllers
       return RedirectToAction("Index");
     }
 
-    // [HttpPost]
-    // public ActionResult DeleteJoin(int ftId)
-    // {
-    //   FlavorTreat joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
-    //   _db.FlavorTreats.Remove(joinEntry);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult DeleteJoin(int ftId)
+    {
+      FlavorTreat flavorTreat = _db.FlavorTreats.FirstOrDefault(ft => ft.FlavorTreatId == ftId);
+      _db.FlavorTreats.Remove(flavorTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
