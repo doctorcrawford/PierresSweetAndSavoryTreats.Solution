019835a0f092213d55b3dc7cc80a5f709cebe9e5
Kyle Crawford <kcraw217@gmail.com>
2023-06-02 10:31:59 -0700

    add property requirement

--- Shop/Models/Flavor.cs
+++ Shop/Models/Flavor.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models;
 
 public class Flavor
 {
   public int FlavorId { get; set; }
+  [Required(ErrorMessage = "You must give the flavor a type!")]
   public string Type { get; set; }
   public List<FlavorTreat> FlavorTreats { get; set; }
 }
--- Shop/Models/Treat.cs
+++ Shop/Models/Treat.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models;
 
 public class Treat
 {
   public int TreatId { get; set; }
+  [Required(ErrorMessage = "You must give a name for the treat!")]
   public string Name { get; set; }
   public List<FlavorTreat> FlavorTreats { get; set; }
 }
